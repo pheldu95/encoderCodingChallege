5854cd6419b86e57d84b57ebcbb812c1c143e62b
Duncan Phelps <phelps.duncan@gmail.com>
2020-09-15 14:14:05 -0500

    somehow the binary is different than the examples given for the challenge.

--- encoder.js
+++ encoder.js
@@ -1,45 +1,34 @@
 
-// function randomInteger(n){
-//     return Math.floor(Math.random() * n);
-// }
-function convertToBinary(array){
-    let binary = "";
-    for (i = 0; i < array.length; i++) {
-        binary += array[i].charCodeAt(0).toString(2) + " "
-    }
-    return binary;
+
+function convertToBinary(string){
+    let binaryString = string.split('').map(function (char) {
+        const binary = char.charCodeAt(0).toString(2)
+        const pad = Math.max(8 - binary.length, 0);
+        // Just to make sure it is 8 bits long.
+        return '0'.repeat(pad) + binary;
+    }).join('');
+    binaryString = binaryString.replace(/ /g, "");
+    return binaryString;
+    // let binary = "";
+    // for (i = 0; i < array.length; i++) {
+    //     binary += array[i].charCodeAt(0).toString(2) + " ";
+        
+    // }
+    // binary = binary.replace(/ /g, "");
+    // //convert the string of binary into an array
+    // let binaryArray = binary.split('');
+    // console.log(binaryArray);
+    // return binaryArray;
 }
-// function encoder(string){
-//     array = string.split('');
-//     //convert the array of characters to binary
-//     let binary = convertToBinary(array);
-//     binary = binary.toString();
-//     //get rid of the spaces.
-//     binary = binary.replace(/ /g, "");
-//     console.log('converted to string', binary);
-//     let binaryArray = binary.split('');
-//     console.log(binaryArray);
-    
-//     for(let i = 0; i < binaryArray.length - 1; i++){
-//         let randomInt = randomInteger(binaryArray.length);       
-//         let charToSwap = binaryArray[i];            
-//         binaryArray[i] = binaryArray[randomInt];
-//         binaryArray[randomInt] = charToSwap;
-//     }
-//     //return the scrambled array
-//     return binaryArray;
-// }
 
-
-// console.log(encoder('Dunc'));
 function binaryScrambler(array){
-    let char1 = array.splice(0, 7);
-    let char2 = array.splice(0, 7);
-    let char3 = array.splice(0, 7);
-    let char4 = array.splice(0, 7);
+    let char1 = array.splice(0, 8);
+    let char2 = array.splice(0, 8);
+    let char3 = array.splice(0, 8);
+    let char4 = array.splice(0, 8);
     console.log(char1, char2, char3, char4);
     let newBinaryArray = [];
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 8; i++) {
         newBinaryArray.push(char1[i], char2[i], char3[i], char4[i]);    
     }
     return newBinaryArray;
@@ -49,14 +38,10 @@ function convertToDec(binaryString) {
         .replace(/[^01]/gi, ''), 2);
 }
 function encoder(string){
-    array = string.split('');
-    //convert the characters in the array to binary
-    let binary = convertToBinary(array);
-    //get rid of the spaces
-    binary = binary.replace(/ /g, "");
-    //convert the string of binary into an array
-    let binaryArray = binary.split('');
-    console.log(binaryArray);
+    //convert the characters in the string to binary
+    let binaryArray = convertToBinary(string).split('');
+    console.log('bionaryarray',binaryArray);
+    
     newBinaryArray = binaryScrambler(binaryArray);
     console.log(newBinaryArray);
     let binaryString = newBinaryArray.toString();
